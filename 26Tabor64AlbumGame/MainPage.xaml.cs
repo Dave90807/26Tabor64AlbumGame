f0c2f94daa43aaad5b540e40ab81f1bb0193305e
Dave Cassidy <Dave90807@msn.com>
2016-06-10 18:57:16 -0700

    Added logic for populating the Album class with random albums.

--- 26Tabor64AlbumGame/MainPage.xaml.cs
+++ 26Tabor64AlbumGame/MainPage.xaml.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using _26Tabor64AlbumGame.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -20,11 +26,106 @@ namespace _26Tabor64AlbumGame
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
+
     public sealed partial class MainPage : Page
     {
+    private ObservableCollection<Song> Songs;
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. Get access to Music Library
+            StorageFolder folder = KnownFolders.MusicLibrary;
+            var allSongs = new ObservableCollection<StorageFile>();
+            await RetrieveFilesInFolders(allSongs, folder);
+
+            // 2. Choose random songs
+            var randomSongs = await PickRandomSongs(allSongs);
+
+            // 3. Get metadata from selected songs.
+            await PopulateSongList(randomSongs);
+
+
+        }
+
+        private async Task RetrieveFilesInFolders(
+            ObservableCollection<StorageFile> list,
+            StorageFolder parent)
+        {
+            foreach (var item in await parent.GetFilesAsync())
+            {
+                if (item.FileType == ".mp3")
+                    list.Add(item);
+            }
+
+            foreach (var item in await parent.GetFoldersAsync())
+            {
+                await RetrieveFilesInFolders(list, item);
+            }
+        }
+
+        private async Task<List<StorageFile>> PickRandomSongs(ObservableCollection<StorageFile> allSongs)
+        {
+            Random random = new Random();
+            var songCount = allSongs.Count;
+            var randomSongs = new List<StorageFile>();
+            
+
+            while (randomSongs.Count < 10)
+            {
+                var randomNumber = random.Next(songCount);
+                var randomSong = allSongs[randomNumber];
+
+                // Find random songs UBT:
+                // 1) Don't pick the same song twice!
+                // 2) Don't pick a song from an album that I've already pciked.
+
+                MusicProperties randomSongMusicProperties =
+                     await randomSong.Properties.GetMusicPropertiesAsync();
+
+                bool isDuplicate = false;
+                foreach (var song in randomSongs)
+                {
+                    MusicProperties songMusicProperties = await song.Properties.GetMusicPropertiesAsync();
+                    if (String.IsNullOrEmpty(randomSongMusicProperties.Album)
+                        || randomSongMusicProperties.Album == songMusicProperties.Album)
+                        isDuplicate = true;
+                }
+
+                if (!isDuplicate)
+                    randomSongs.Add(randomSong);
+
+            }
+            return randomSongs;
+        }
+
+        private async Task PopulateSongList(List<StorageFile> files)
+        {
+            int id = 0;
+            foreach (var file in files)
+            {
+                MusicProperties songProperties = await file.Properties.GetMusicPropertiesAsync();
+                StorageItemThumbnail currentThumb = await file.GetThumbnailAsync(
+                    ThumbnailMode.MusicView, 200, ThumbnailOptions.UseCurrentScale);
+
+                var albumCover = new BitmapImage();
+                albumCover.SetSource(currentThumb);
+
+                var song = new Song();
+                song.Id = id;
+                song.Title = songProperties.Title;
+                song.Artist = songProperties.Artist;
+                song.Album = songProperties.Album;
+                song.AlbumCover = albumCover;
+                song.SongFile = file;
+                Songs.Add(song);
+                id++;
+            }
         }
     }
 }
