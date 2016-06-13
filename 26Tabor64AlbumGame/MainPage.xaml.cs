932ddcccd48325633e14eb4a860744ca328ee9f4
Dave Cassidy <Dave90807@msn.com>
2016-06-12 21:58:50 -0700

    All play functions work.  Only ads are missing.

--- 26Tabor64AlbumGame/MainPage.xaml.cs
+++ 26Tabor64AlbumGame/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -31,6 +32,9 @@ namespace _26Tabor64AlbumGame
     {
         private ObservableCollection<Song> Songs;
         private ObservableCollection<StorageFile> AllSongs;
+        bool _playingMusic = false;
+        int _round = 0;
+        int _totalScore = 0;
 
         public MainPage()
         {
@@ -113,40 +117,98 @@ namespace _26Tabor64AlbumGame
             }
         }
 
+        private async void SongGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Ignor clicks when we are in cooldown
+            if (!_playingMusic) return;
+
+            CountDown.Pause();
+            MyMediaElement.Stop();
+
+            var clickedSong = (Song)e.ClickedItem;
+            var correctSong = Songs.FirstOrDefault(p => p.Selected == true);
+
+            // Evaluate the user's selection
+            Uri uri;
+            int score;
+            if (clickedSong.Selected)
+            {
+                uri = new Uri("ms-appx:///Assets/correct.png");
+                score = (int)MyProgressBar.Value;
+            }
+            else
+            {
+                uri = new Uri("ms-appx:///Assets/incorrect.png");
+                score = (int)MyProgressBar.Value * -1;
+            }
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+            var fileStream = await file.OpenAsync(FileAccessMode.Read);
+            await clickedSong.AlbumCover.SetSourceAsync(fileStream);
+
+            _totalScore += score;
+            _round++;
+
+            ResultTextblock.Text = String.Format("Score: {0} Total Score after {1} Rounds: {2}", score, _round, _totalScore);
+            TitleTextblock.Text = String.Format("Correct Song: {0}", correctSong.Title);
+            ArtistTextblock.Text = String.Format("Performed by: {0}", correctSong.Artist);
+            AlbumTextblock.Text = String.Format("On Album: {0}", correctSong.Album);
+
+            clickedSong.Used = true;
+            correctSong.Selected = false;
+            correctSong.Used = true;
+
+            if (_round >= 5)
+            {
+                InstructionTextBlock.Text = string.Format("Game over... You scored: {0}", _totalScore);
+                PlayAgainButton.Visibility = Visibility.Visible;
+            }else
+            {
+            StartCooldown();
+            }
+        }
+
+        private async void PlayAgainButton_Click(object sender, RoutedEventArgs e)
+        {
+            await PrepareNewGame();
+            PlayAgainButton.Visibility = Visibility.Collapsed;
+        }
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void SongGridView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-
-        }
-
-        private void PlayAgainButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
         private async Task<ObservableCollection<StorageFile>> SetupMusicList()
         {
-        //  Get access to Music Library
-        StorageFolder folder = KnownFolders.MusicLibrary;
-        var allSongs = new ObservableCollection<StorageFile>();
-        await RetrieveFilesInFolders(allSongs, folder);
-        return allSongs;
+            //  Get access to Music Library
+            // StorageFolder folder = KnownFolders.MusicLibrary;
+            string path = "C:\\Users\\DaveC\\Music\\BobsFakeMusicLibrary";
+            StorageFolder bobsMusicFolder = await StorageFolder.GetFolderFromPathAsync(path);
+            var allSongs = new ObservableCollection<StorageFile>();
+            await RetrieveFilesInFolders(allSongs, bobsMusicFolder);  // replaced "folder" w "bobsMusicFolder"
+            return allSongs;
         }
 
         private async Task PrepareNewGame()
         {
-        Songs.Clear();
-        // Choose random songs from library
+            Songs.Clear();
+            // Choose random songs from library
             var randomSongs = await PickRandomSongs(AllSongs);
 
-        //  Get metadata from selected songs.
+            //  Get metadata from selected songs.
             await PopulateSongList(randomSongs);
 
-        // State management
+            StartCooldown();
+
+            // State management
+            InstructionTextBlock.Text = "Get ready ...";
+            ResultTextblock.Text = "";
+            TitleTextblock.Text = "";
+            ArtistTextblock.Text = "";
+            AlbumTextblock.Text = "";
+
+            _totalScore = 0;
+            _round = 0;
         }
 
         private async void Grid_Loaded(object sender, RoutedEventArgs e)
@@ -157,6 +219,53 @@ namespace _26Tabor64AlbumGame
             await PrepareNewGame();
 
             StartupProgressRing.IsActive = false;
+
+            StartCooldown();
         }
+
+        private void StartCooldown()
+        {
+            _playingMusic = false;
+            SolidColorBrush brush = new SolidColorBrush(Colors.Blue);
+            MyProgressBar.Foreground = brush;
+            InstructionTextBlock.Text = string.Format("Get ready for round {0}...", _round + 1);
+            InstructionTextBlock.Foreground = brush;
+            CountDown.Begin();
+        }
+
+        private void StartCountdown()
+        {
+            _playingMusic = true;
+            SolidColorBrush brush = new SolidColorBrush(Colors.Red);
+            MyProgressBar.Foreground = brush;
+            InstructionTextBlock.Text = "Go!";
+            InstructionTextBlock.Foreground = brush;
+            CountDown.Begin();
+        }
+
+        private async void CountDown_Completed(object sender, object e)
+        {
+            if (!_playingMusic)
+            {
+                // Start playing music
+                var song = PickSong();
+                MyMediaElement.SetSource(
+                    await song.SongFile.OpenAsync(FileAccessMode.Read),
+                    song.SongFile.ContentType);
+                // Start countdown
+                StartCountdown();
+            }
+        }
+
+        private Song PickSong()
+        {
+            Random random = new Random();
+            var unusedSongs = Songs.Where(p => p.Used == false);
+            var randomNumber = random.Next(unusedSongs.Count());
+            var randomSong = unusedSongs.ElementAt(randomNumber);
+            randomSong.Selected = true;
+            return randomSong;
+        }
+
     }
 }
